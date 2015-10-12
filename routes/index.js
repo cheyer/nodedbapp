b442a9155671e452cb153d6db369b45466f5285b
Chris Heyer <cheyer03@gmail.com>
2015-10-12 11:02:21 +0200

    added functionality to see database contents on index page

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,20 @@
+var nano = require('nano')('http://localhost:5984');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  res.render('index', { title: 'Express' });
+  var db = nano.db.use('personen');
+  db.get('einzigartigeID', function(err, body){
+    if(!err){
+      var list = body.personlist;
+      //console.log(list);
+      res.render('index', {title: 'Express', "personen":list});
+    }else{
+      console.log("Error in getting document from database");
+      res.render('index', { title: 'Express' });
+    }
+  });
 });
 
 module.exports = router;
